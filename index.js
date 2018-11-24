ad5fca53c29c48498ac4738fb6e4ac8aca2dd21f
dev-seack <dev@ts-dm.de>
2018-11-24 23:49:40 +0100

    changed view engine to pug <3

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const nodemailer = require("nodemailer");
-const excel = require('excel4node');
-const nib = require('nib');
-const stylus = require('stylus');
+const excel = require("excel4node");
+const nib = require("nib");
+const stylus = require("stylus");
 
 // port
 const PORT = 3000;
@@ -13,21 +13,30 @@ const PORT = 3000;
 const app = express();
 
 // set view engine
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
 
 // bodyparser
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // stylus
-app.use(stylus.middleware({
-    src: __dirname + '/public',
-    compile: (str, path) => stylus(str)
-        .set('filename', path)
+app.use(
+  stylus.middleware({
+    src: __dirname + "/public",
+    compile: (str, path) =>
+      stylus(str)
+        .set("filename", path)
         .use(nib())
-}));
+  })
+);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
 
 // listen
 app.listen(PORT, () => {
-    console.log(`Server is running on ${3000}`);
+  console.log(`Server is running on ${PORT}`);
 });
