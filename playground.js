8f59d4ba619ed69262f184a4abd3fff2d02b0989
dev-seack <dev@ts-dm.de>
2018-11-26 17:42:17 +0100

    Some playground stuff with the CRM API

---
+++ playground.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+const url =
+  "https://auftragsdepot24.centralstationcrm.net/api/people.json?apikey=vjt5m7gmfnl31d0c7m0vko5imx8pwl";
+const data = {
+  name: "Seack",
+  first_name: "Marius"
+};
+
+axios
+  .post(url, data)
+  .then((res) => {
+    console.log(`status: ${res.status}`);
+    console.log(res.data.person);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
