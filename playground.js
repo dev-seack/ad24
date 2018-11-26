5e5ee51efc243c9913107a76ad97911ecd75330b
dev-seack <dev@ts-dm.de>
2018-11-26 17:42:59 +0100

    one quick change

--- playground.js
+++ playground.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const url =
-  "https://auftragsdepot24.centralstationcrm.net/api/people.json?apikey=vjt5m7gmfnl31d0c7m0vko5imx8pwl";
+  "https://auftragsdepot24.centralstationcrm.net/api/people.json?apikey=vjt5m7gmfnl31d0c7m0vko5imx8pwl"; // API KEY in config
 const data = {
   name: "Seack",
   first_name: "Marius"
